0732daf4a8e04ec3bfa1914609991a30ce99cf8d
Zain Ahmed <zain.ahmed@ada.ac.uk>
2024-10-30 00:33:13 +0000

    Added main logic to the fileExists and validNumber functions

--- dfr.js
+++ dfr.js
@@ -1,55 +1,39 @@
 const fs = require("fs");
 
 function fileExists(filename) {
-  return fs.existsSync(filename);
+	return fs.existsSync(filename);
 }
 
 function validNumber(value) {
-  
+	const regex = /^-?\d+(\.\d+)?$/;
+	return regex.test(value);
 }
 
-function dataDimensions(dataframe) {
+function dataDimensions(dataframe) {}
 
-}
+function findTotal(dataset) {}
 
-function findTotal(dataset) {
-  
-}
+function calculateMean(dataset) {}
 
-function calculateMean(dataset) {
-  
-}
+function calculateMedian(dataset) {}
 
-function calculateMedian(dataset) {
+function convertToNumber(dataframe, col) {}
 
-}
+function flatten(dataframe) {}
 
-function convertToNumber(dataframe, col) {
+function loadCSV(csvFile, ignoreRows, ignoreCols) {}
 
-}
-
-function flatten(dataframe) {
-
-}
-
-function loadCSV(csvFile, ignoreRows, ignoreCols) {
-
-}
-
-
-function createSlice(dataframe, columnIndex, pattern, exportColumns = []) {
-
-}
+function createSlice(dataframe, columnIndex, pattern, exportColumns = []) {}
 
 module.exports = {
-  fileExists,
-  validNumber,
-  dataDimensions,
-  calculateMean,
-  findTotal,
-  convertToNumber,
-  flatten,
-  loadCSV,
-  calculateMedian,
-  createSlice,
+	fileExists,
+	validNumber,
+	dataDimensions,
+	calculateMean,
+	findTotal,
+	convertToNumber,
+	flatten,
+	loadCSV,
+	calculateMedian,
+	createSlice,
 };
